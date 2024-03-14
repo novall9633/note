eb367ae67ecfe94a118a3f8f96f95b68fb8525d6
신용진 <novall9633@naver.com>
2024-03-14 18:03:59 +0900

    메모 삭제 API 추가

--- notion-clone/app/(main)/_components/navigation.tsx
+++ notion-clone/app/(main)/_components/navigation.tsx
@@ -1,18 +1,23 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useQuery } from "convex/react";
+import { useMutation} from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { toast } from "sonner";
+import { DocumentList } from "./document-list";
+import { Popover, PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
-    const documents = useQuery(api.documents.get);
+
+    const create = useMutation(api.documents.create);
     const isResizingRef = useRef(false);
     const sidebarRef=useRef<ElementRef<"aside">>(null);
     const navbarRef=useRef<ElementRef<"div">>(null);
@@ -84,6 +89,15 @@ export const Navigation = () => {
             }, 300);
         }
     }
+
+    const handleCreate = () =>{
+        const promise = create({title:"Untitled"});
+        toast.promise(promise,{
+            loading: "Creating a new note...",
+            success:"New note created",
+            error:"Failed to create a new note.",
+        });
+    };
     
     return (
         <>
@@ -95,11 +109,19 @@ export const Navigation = () => {
                 </div>
                 <div>
                     <UserItem />
+                    <Item label="Search" icon={Search} isSearch onClick={()=>{}} />
+                    <Item label="Settings" icon={Settings} onClick={()=>{}} />
+                    <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
                 </div>
                 <div className="mt-4">
-                    {documents?.map((document)=>(
-                        <p key={document._id}>{document.title}</p>
-                    ))}
+                    <DocumentList />
+                    <Item onClick={handleCreate} icon={Plus} label="Add a page" />
+                    <Popover>
+                        <PopoverTrigger className="w-full mt-4">
+                            <Item label="Trash" icon={Trash} />
+                        </PopoverTrigger>
+                        <PopoverContent className="p-0 w-72" side={isMobile? "bottom":"right"} />
+                    </Popover>
                 </div>
                 <div 
                 onMouseDown={handleMouseDown} onClick={resetWidth} className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0">
--- notion-clone/convex/documents.ts
+++ notion-clone/convex/documents.ts
@@ -2,23 +2,89 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
-export const get = query({
-    handler: async (ctx) =>{
-        const identity = await ctx.auth.getUserIdentity();
-
-        if(!identity){
-            throw new Error("Not authenticated");
+export const archive = mutation({
+    args: { id: v.id("document") },
+    handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+  
+      if (!identity) {
+        throw new Error("Not authenticated");
+      }
+  
+      const userId = identity.subject;
+  
+      const existingDocument = await ctx.db.get(args.id);
+  
+      if (!existingDocument) {
+        throw new Error("Not found");
+      }
+  
+      if (existingDocument.userId !== userId) {
+        throw new Error("Unauthorized");
+      }
+  
+      const recursiveArchive = async (documentId: Id<"document">) => {
+        const children = await ctx.db
+          .query("document")
+          .withIndex("by_user_parent", (q) => (
+            q
+              .eq("userId", userId)
+              .eq("parentDocument", documentId)
+          ))
+          .collect();
+  
+        for (const child of children) {
+          await ctx.db.patch(child._id, {
+            isArchived: true,
+          });
+  
+          await recursiveArchive(child._id);
         }
-
-        const documents = await ctx.db.query("documents").collect();
-
-        return documents;
+      }
+  
+      const document = await ctx.db.patch(args.id, {
+        isArchived: true,
+      });
+  
+      recursiveArchive(args.id);
+  
+      return document;
     }
-})
+  })
+  
+  export const getSidebar = query({
+    args: {
+      parentDocument: v.optional(v.id("document"))
+    },
+    handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+  
+      if (!identity) {
+        throw new Error("Not authenticated");
+      }
+  
+      const userId = identity.subject;
+  
+      const documents = await ctx.db
+        .query("document")
+        .withIndex("by_user_parent", (q) =>
+          q
+            .eq("userId", userId)
+            .eq("parentDocument", args.parentDocument)
+        )
+        .filter((q) =>
+          q.eq(q.field("isArchived"), false)
+        )
+        .order("desc")
+        .collect();
+  
+      return documents;
+    },
+  });
 export const create = mutation({
     args: {
         title: v.string(),
-        parentDocument: v.optional(v.id("documents")),
+        parentDocument: v.optional(v.id("document")),
     },
     handler: async (ctx, args) => {
         const identity = await ctx.auth.getUserIdentity();
@@ -29,7 +95,7 @@ export const create = mutation({
 
         const userId = identity.subject;
 
-        const document = await ctx.db.insert("documents", {
+        const document = await ctx.db.insert("document", {
             title: args.title,
             parentDocument: args.parentDocument,
             userId,
@@ -40,3 +106,84 @@ export const create = mutation({
         return document;
     },
 });
+export const getTrash = query({
+    handler: async (ctx) => {
+      const identity = await ctx.auth.getUserIdentity();
+  
+      if (!identity) {
+        throw new Error("Not authenticated");
+      }
+  
+      const userId = identity.subject;
+  
+      const documents = await ctx.db
+        .query("document")
+        .withIndex("by_user", (q) => q.eq("userId", userId))
+        .filter((q) =>
+          q.eq(q.field("isArchived"), true),
+        )
+        .order("desc")
+        .collect();
+  
+      return documents;
+    }
+  });
+
+  export const restore = mutation({
+    args: { id: v.id("documents") },
+    handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+  
+      if (!identity) {
+        throw new Error("Not authenticated");
+      }
+  
+      const userId = identity.subject;
+  
+      const existingDocument = await ctx.db.get(args.id);
+  
+      if (!existingDocument) {
+        throw new Error("Not found");
+      }
+  
+      if (existingDocument.userId !== userId) {
+        throw new Error("Unauthorized");
+      }
+  
+      const recursiveRestore = async (documentId: Id<"document">) => {
+        const children = await ctx.db
+          .query("document")
+          .withIndex("by_user_parent", (q) => (
+            q
+              .eq("userId", userId)
+              .eq("parentDocument", documentId)
+          ))
+          .collect();
+  
+        for (const child of children) {
+          await ctx.db.patch(child._id, {
+            isArchived: false,
+          });
+  
+          await recursiveRestore(child._id);
+        }
+      }
+  
+      const options: Partial<Doc<"document">> = {
+        isArchived: false,
+      };
+  
+      if (existingDocument.parentDocument) {
+        const parent = await ctx.db.get(existingDocument.parentDocument);
+        if (parent?.isArchived) {
+          options.parentDocument = undefined;
+        }
+      }
+  
+      const document = await ctx.db.patch(args.id, options);
+  
+      recursiveRestore(args.id);
+  
+      return document;
+    }
+  });
