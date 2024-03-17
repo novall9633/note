1220d70ba72f245eab8b84a1e9111e12e966456f
신용진 <novall9633@naver.com>
2024-03-17 21:58:19 +0900

    page,layout 추가 이미지 삭제 안되던거 수정

--- notion-clone/components/editor.tsx
+++ notion-clone/components/editor.tsx
@@ -27,7 +27,7 @@ const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
 
     const editor: BlockNoteEditor = useBlockNote({
         editable,
-        initialContent: initialContent ? (JSON.parse(initialContent) as PartialBlock[]) : undefined,
+        initialContent: initialContent ? JSON.parse(initialContent) as PartialBlock[] : undefined,
         onEditorContentChange: (editor) => {
             onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
         },
